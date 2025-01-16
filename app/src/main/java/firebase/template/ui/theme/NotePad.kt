d79d08a91e086398e4ee8fe5e179756ba0b1f511
Brian Salomone <brian.j.salomone@gmail.com>
2025-01-15 18:51:36 -0800

    more formatting changes

--- app/src/main/java/firebase/template/ui/theme/NotePad.kt
+++ app/src/main/java/firebase/template/ui/theme/NotePad.kt
@@ -2,7 +2,9 @@ package firebase.template.ui.theme
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import firebase.template.R
 import firebase.template.RegText
@@ -47,10 +50,16 @@ class NotePad {
                 defaultElevation = 5.dp
             ),
         ) {
-            Column (modifier = Modifier.padding(6.dp)){
-                RegText(text = note[i].title)
-                RegText(text = note[i].body)
-                RegText(text = note[i].lastEdited)
+            Column (modifier = Modifier
+                .fillMaxSize()
+                .padding(6.dp)){
+                RegText(text = note[i].title, fontSize = 18, fontWeight = FontWeight.Bold)
+                RegText(text = note[i].body, fontSize = 15)
+                Row(modifier = Modifier
+                    .fillMaxSize(),
+                    horizontalArrangement = Arrangement.End) {
+                    RegText(text = note[i].lastEdited, fontSize = 15)
+                }
             }
 
         }
