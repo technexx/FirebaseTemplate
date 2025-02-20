24dff99a9b66a657908870ca0a7a7f0d301dc06c
Brian Salomone <brian.j.salomone@gmail.com>
2025-02-20 15:22:09 -0800

    body text field now covers full composables and grabs focus on clicks

--- app/src/main/java/firebase/template/NotePad.kt
+++ app/src/main/java/firebase/template/NotePad.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -52,8 +53,11 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -274,6 +278,8 @@ class NotePad(private val viewModel: ViewModel, private val roomInteraction: Roo
     fun AddNoteScreen() {
         var titleTxtField by remember { mutableStateOf("Untitled") }
         var bodyTxtField by remember { mutableStateOf("") }
+        val focusRequester = remember { FocusRequester() }
+        val focusManager = LocalFocusManager.current
         val coroutineScope = rememberCoroutineScope()
 
         AnimatedComposable(
@@ -314,26 +320,36 @@ class NotePad(private val viewModel: ViewModel, private val roomInteraction: Roo
 
                 HorizontalDivider(color = colorResource(id = Theme.themeColorsList[viewModel.getColorTheme].primaryColor), thickness = 2.dp)
 
-                TextField(modifier = Modifier,
-                    keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
-                    value = bodyTxtField,
-                    placeholder = { Text(showPlaceHolderTextIfFieldIsEmpty(bodyTxtField, "Note")) },
-                    onValueChange = {
-                        bodyTxtField = it
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .clickable {
+                        focusManager.clearFocus() // Clear focus first
+                        focusRequester.requestFocus()
                     },
-                    singleLine = true,
-                    textStyle = TextStyle(color = colorResource(id = Theme.themeColorsList[viewModel.getColorTheme].noteText), fontSize = 16.sp),
+                    contentAlignment = Alignment.TopStart,
+                    ) {
+                    TextField(modifier = Modifier
+                        .focusRequester(focusRequester),
+                        keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
+                        value = bodyTxtField,
+                        placeholder = { Text(showPlaceHolderTextIfFieldIsEmpty(bodyTxtField, "Note")) },
+                        onValueChange = {
+                            bodyTxtField = it
+                        },
+                        singleLine = true,
+                        textStyle = TextStyle(color = colorResource(id = Theme.themeColorsList[viewModel.getColorTheme].noteText), fontSize = 16.sp),
 
-                    colors = TextFieldDefaults.colors(
-                        cursorColor = colorResource(id = Theme.themeColorsList[viewModel.getColorTheme].textFieldCursorColor),
-                        focusedContainerColor = colorResource(id = Theme.themeColorsList[viewModel.getColorTheme].textFieldColor),
-                        unfocusedContainerColor = colorResource(id = Theme.themeColorsList[viewModel.getColorTheme].textFieldColor),
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent,
-                        focusedPlaceholderColor = colorResource(id = Theme.themeColorsList[viewModel.getColorTheme].textFieldPlaceHolderTextColor),
-                        unfocusedPlaceholderColor = colorResource(id = Theme.themeColorsList[viewModel.getColorTheme].textFieldPlaceHolderTextColor),
+                        colors = TextFieldDefaults.colors(
+                            cursorColor = colorResource(id = Theme.themeColorsList[viewModel.getColorTheme].textFieldCursorColor),
+                            focusedContainerColor = colorResource(id = Theme.themeColorsList[viewModel.getColorTheme].textFieldColor),
+                            unfocusedContainerColor = colorResource(id = Theme.themeColorsList[viewModel.getColorTheme].textFieldColor),
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedPlaceholderColor = colorResource(id = Theme.themeColorsList[viewModel.getColorTheme].textFieldPlaceHolderTextColor),
+                            unfocusedPlaceholderColor = colorResource(id = Theme.themeColorsList[viewModel.getColorTheme].textFieldPlaceHolderTextColor),
+                        )
                     )
-                )
+                }
             }
         }
     }
